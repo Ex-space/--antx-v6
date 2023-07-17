1a08ea4b91a04778a7eeb672f79793a74418cd74
liuye <2716235669@qq.com>
2023-07-18 00:07:12 +0800

    修改emptynode的样式，调整线高度，更改角阀处字样

--- src/nodes/emptyNode.ts
+++ src/nodes/emptyNode.ts
@@ -1,42 +1,28 @@
-import { Markup } from "@antv/x6";
-import { reactive, ref } from "vue";
 export const emptyNode1 = {
-  x: 838,
-  width: 46,
-  height: 30,
+  x: 810,
+  width: 110,
+  height: 20,
   shape: 'html',
-  html: `
-          <div class="emptyNodeSafe">
-            <div></div>
-          </div>
-          `,
-  attrs: {
-    // 一定要给边框宽度加上>0的值,否则节点将不能连线
-    body: {
-      stroke: 'transparent',
-      strokeWidth: 1, // 边框的粗细
-      magnet: true, // 节点是否可以连线
-    }
-  },
+      attrs: {
+        // 一定要给边框宽度加上>0的值,否则节点将不能连线
+        body: {
+          stroke: 'transparent',
+          strokeWidth: 1, // 边框的粗细
+          magnet: true, // 节点是否可以连线
+        }
+      },
 };
 export const emptyNode2 = {
-  x: 934,
-  width: 46,
-  height: 30,
+  x: 950,
+  width: 110,
+  height: 20,
   shape: 'html',
-  html: `
-          <div class="emptyNodeWarnFB">
-            <div></div>
-            <div></div>
-            <div></div>
-          </div>
-          `,
-  attrs: {
-    // 一定要给边框宽度加上>0的值,否则节点将不能连线
-    body: {
-      stroke: 'transparent',
-      strokeWidth: 1, // 边框的粗细
-      magnet: true, // 节点是否可以连线
-    }
-  },
+      attrs: {
+        // 一定要给边框宽度加上>0的值,否则节点将不能连线
+        body: {
+          stroke: 'transparent',
+          strokeWidth: 1, // 边框的粗细
+          magnet: true, // 节点是否可以连线
+        }
+      },
 };
