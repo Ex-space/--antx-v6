62758060d25a4ae8bd66b652434a663d66deeef2
liuye <2716235669@qq.com>
2023-08-21 18:13:27 +0800

    增加了闪烁，对home页面接口

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,8 @@ import axios from "axios";
 import App from "./App.vue";
 import router from './router'
 import piniaPersist from 'pinia-plugin-persist'
-// axios.defaults.baseURL = "http://localhost:8089/jydam";
-axios.defaults.baseURL = "http://118.178.132.49:8089/jydam"
+axios.defaults.baseURL = "http://localhost:8089/jydam";
+// axios.defaults.baseURL = "http://118.178.132.49:8089/jydam"
 // http://localhost:8089/jydam/getDeviceDataBetweenTimeByHour
 const app = createApp(App);
 const pinia = createPinia();
